56112f4975ae6c268f5f55106679de64883a775c
dubrovskyi <o.dubrovskyi@codetiburon.com>
2020-09-12 11:16:55 +0300

    init app

---
+++ app/Http/Controllers/Api/TasksController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Class TasksController
+ *
+ * @package App\Http\Controllers
+ */
+class TasksController extends Controller
+{
+    public function getGroupedList(Request $request)
+    {
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $company = Task::findOrFail($id);
+        $company->update($request->all());
+
+        return $company;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $company = Task::findOrFail($id);
+        $company->delete();
+
+        return new \Illuminate\Http\Response();
+    }
+}
